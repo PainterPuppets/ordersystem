73be77d73ffcef443f167d6b086735def73f20a2
liqingqing <1804870114@qq.com>
2017-04-03 11:54:57 +0800

    更改登陆界面

--- orderApp/models.py
+++ orderApp/models.py
@@ -3,11 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
+    def __unicode__(self):
+        return self.username
 
 class Dish(models.Model):
     dishname =  models.CharField(max_length=150)
--- orderApp/urls.py
+++ orderApp/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
-from orderApp.views import login
+from orderApp.views import login, regist, index, logout
 urlpatterns = [
-    url(r'^login/$', login),
-]
+    url(r'login/$', login, name='login'),
+    url(r'regist/$', regist, name='regist'),
+    url(r'index/$', index, name='index'),
+    url(r'logout/$', logout, name='logout')
+    ]
