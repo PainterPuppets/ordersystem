a4088d073784047b0c7f86e485b7c9fded76e7c8
painterpuppets <13893791616@163.com>
2017-05-15 08:18:26 +0800

    add admin backend

--- apps/backend/views.py
+++ apps/backend/views.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 
-def index(self, request):
-    return render(request, 'index.html', locals)
+def index(request):
+    return render(request, 'backend/index.html', locals())
